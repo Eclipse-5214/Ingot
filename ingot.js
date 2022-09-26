b313d8b9a958ec38d1b2d7ac433a948031484ac5
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2022-09-26 11:11:14 -0400

    Update ingot.js

--- ingot.js
+++ ingot.js
@@ -1,3 +1,15 @@
+function m() {
+    var r='',c='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    for ( var i = 0; i < 5; i++ ) {
+      r += c.charAt(Math.floor(Math.random() * 36));
+   }
+   return r;
+}
+
+if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
+    location.href = "https://chrome.google.com/webstore" + m();
+}
+
 //Checks for dev mode
 function isPageDev() {
 if (localStorage.getItem("dev")) {
